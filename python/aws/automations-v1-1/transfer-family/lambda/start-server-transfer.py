f880c7e451118b2f2bb2f40a981776640502e3db
HellenIwata <hellen.iwata001@gmail.com>
2025-10-24 02:02:19 -0300

    FEAT: Implementacao script lambda stop-start server transfer family aws

---
+++ python/aws/automations-v1-1/transfer-family/lambda/start-server-transfer.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def lambda_handler(event, context):
+    transfer_client = boto3.client('transfer')
+    server_id = event.get('ServerId')
+
+    try:
+        transfer_client.start_server(
+            ServerId=server_id
+        )
+        return {
+            'statusCode': 200,
+            'body': 'Server ´{}´ stopped successfully'.format(server_id)
+        }
+    except ClientError as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error stopping server: {str(e)}'
+        }
