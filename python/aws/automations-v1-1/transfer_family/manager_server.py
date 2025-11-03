e20066c809b8f17ef030bb72049d8119fe2f3dd8
HellenIwata <hellen.iwata001@gmail.com>
2025-11-03 01:18:09 -0300

    feat(manager-server): ajuste das funções necessarias para executar o arquivo.
    Adicionando a politica padrão para a criação/atualização de usuario;\nAdicionando as funcoes de logica para criação e atualização de usuario;\ncriando apenas funções unicas para criar/delete/atualizar usuario com opcao de fazer para mais de 1 usuario

--- python/aws/automations-v1-1/transfer_family/manager_server.py
+++ python/aws/automations-v1-1/transfer_family/manager_server.py
@@ -21,6 +21,52 @@ def show_menu_transfer():
 
 transfer_client = boto3.client('transfer')
 
+policy_document = """
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowReadAcess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+            ],
+            "Resource": [
+                "arn:aws:s3:::meu-bucket/meu-objeto/*"
+            ]
+        },
+        {
+            "Sid": "AllowReadAcessAplication",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:PutObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::meu-bucket/meu-objeto/*"
+            ],
+            "Condition": {
+                "StringLikeIfExists": {
+                    "aws:username": "*_motor"
+                }
+            }
+        },
+        {
+            "Sid": "AllowListBucket",
+            "Effect": "Allow",
+            "Action": "s3:ListBucket",
+            "Resource": [
+                "arn:aws:s3:::meu-bucket"
+            ],
+        }
+    ]
+}
+"""
+
+# Functions Scopes
 def scope_endpoint():
     print("""
 ============ SERVER ENDPOINT SCOPE ============
@@ -62,6 +108,64 @@ def scope_protocol():
     }
     return mapping.get(choice, 'invalid choice')
 
+# Functions Logics
+def _create_user_logic(server_id, user_name, role_arn, directory_mapping,ssh_key,tag_customer):
+    try:
+        transfer_client.create_user(
+            ServerId=server_id,
+            UserName=user_name,
+            RoleArn=role_arn,
+            Policy=policy_document,
+            HomeDirectoryMappings=[{
+                'Entry':'/',
+                'Target': directory_mapping
+            }],
+            Tags=[
+                {
+                    'Key': 'customer',
+                    'Value': tag_customer
+                }
+            ]
+        )
+
+        transfer_client.import_ssh_public_key(
+            ServerId=server_id,
+            UserName=user_name,
+            PublicKey=ssh_key
+        )
+
+        print("User {} created successfully on Server {}".format(user_name, server_id))
+    except Exception as e:
+        print("Error creating user: {}".format(e))
+
+
+def _update_user_logic(server_id, user_name, role_arn, home_directory):
+    try:
+        transfer_client.update_user(
+            ServerId=server_id,
+            UserName=user_name,
+            RoleArn=role_arn,
+            Policy=policy_document,
+            HomeDirectoryMappings=[{
+                'Entry':'/',
+                'Target': home_directory
+            }]
+        )
+        print("User {} updated successfully on Server {}".format(user_name, server_id))
+    except Exception as e:
+        print("Error updating user: {}".format(e))
+
+
+def _delete_user_logic(server_id, user_name):
+    try:
+        transfer_client.delete_user(
+            ServerId=server_id,
+            UserName=user_name
+        )
+        print("User {} deleted successfully from Server {}".format(user_name, server_id))
+    except Exception as e:
+        print("Error deleting user: {}".format(e))
+
 
 def create_server():
     endpoint_type = scope_endpoint()
@@ -83,55 +187,101 @@ def create_server():
     except Exception as e:
         print("Error creating server: {}".format(e))
 
-
-def create_user():
+""" def create_user():
     server_id = input("Enter server ID: ").strip()
     user_name = input("Enter user name: ").strip()
     role_arn = input("Enter role ARN: ").strip()
     policy_arn = input("Enter policy ARN: ").strip()
-    home_directory = input("Enter home directory (e.g., /my-bucket/user): ").strip()
+    directory_mapping = input("Enter home directory (e.g., /my-bucket/user): ").strip()
     ssh_key = input("Enter SSH key: ")
 
-    if not server_id or not user_name or not role_arn or not policy_arn or not home_directory or not ssh_key:
+    if not server_id or not user_name or not role_arn or not policy_arn or not directory_mapping or not ssh_key:
         print("All fields are required.")
         return
     
-    try:
-        transfer_client.create_user(
-            ServerId=server_id,
-            UserName=user_name,
-            RoleArn=role_arn,
-            PolicyArn=policy_arn,
-            HomeDirectory=home_directory,
-        )
-
-        transfer_client.import_ssh_public_key(
-            ServerId=server_id,
-            UserName=user_name,
-            PublicKey=ssh_key
-        )
-
-        print("User {} created successfully on Server {}".format(user_name, server_id))
-    except Exception as e:
-        print("Error creating user: {}".format(e))
+    _create_user_logic(server_id, user_name, role_arn, policy_arn, directory_mapping, ssh_key) """
 
 
-def delete_user():
-    server_id = input("Enter Server ID: ").strip()
-    user_name = input("Enter User Name: ").strip()
+def create_multiple_users(server_id, user_name, role_arn, directory_mapping,ssh_key,tag_customer):
+    print("Creating user '{}' on server '{}'...".format(user_name, server_id))
+    _create_user_logic(server_id, user_name, role_arn, directory_mapping,ssh_key,tag_customer)
 
-    if not server_id or not user_name:
-        print("Both Server ID and User Name are required.")
-        return
+
+def update_multiple_users(server_id, user_name, role_arn, home_directory):
+    print("Updating the user '{}' on server '{}'...".format(user_name, server_id))
+    _update_user_logic(server_id, user_name, role_arn, home_directory)
+
+
+def delete_multiple_users(server_id, user_name):
+    print("Deleting the user '{}' from server '{}'...".format(user_name, server_id))
+    _delete_user_logic(server_id, user_name)
+
+
+def create_users():
+    server_id = input("Enter server ID: ").strip()
+    users_name = []
+    directory_mappings=[]
+    ssh_keys=[]
+    tag_customers=[]
+
+    while True:
+        user_name = input("Enter a user name: ").lower()
+        users_name.append(user_name)
+
+        directory_mapping = input("Enter Home Directory (e.g., /my-bucket/user): ").strip()
+        directory_mappings.append(directory_mapping)
+
+        ssh_key = input("Enter SSH Key: ").strip()
+        ssh_keys.append(ssh_key)
+
+        tag_customer = input("Enter name Customer for TAG: ").strip()
+        tag_customers.append(tag_customer)
+
+        more = input("Do you want to add another user? (y/n): ")
+        if more.lower() != 'y':
+            break
     
-    try:
-        transfer_client.delete_user(
-            ServerId=server_id,
-            UserName=user_name
-        )
-        print("User {} deleted successfully from Server {}".format(user_name, server_id))
-    except Exception as e:
-        print("Error deleting user: {}".format(e))
+    role_arn = input("Enter Role ARN: ").strip()
+
+    for user, directory_mapping, ssh_key, tag_customer in zip(users_name, directory_mappings, ssh_keys, tag_customers):
+        create_multiple_users(server_id, user, role_arn, directory_mapping, ssh_key, tag_customer)
+
+
+def update_users():
+    server_id = input("Enter Server ID: ").strip()
+    users_name = []
+    directory_mappings=[]
+    while True:
+        user_name = input("Enter a user name: ").lower()
+        users_name.append(user_name)
+
+        directory_mappings=input("Enter Home Directory (e.g., /my-bucket/user): ").strip()
+        directory_mappings.append(directory_mappings)
+
+        more = input("Do you want to add another user? (y/n): ")
+        if more.lower() != 'y':
+            break
+    
+    role_arn = input("Enter Role ARN: ").strip()
+
+    for user, directory_mapping in zip(users_name, directory_mappings):
+        update_multiple_users(server_id, user, role_arn, directory_mapping)
+
+
+def delete_users():
+    server_id = input("Enter Server ID: ").strip()
+    users_name = []
+
+    while True:
+        users_name = input("Enter a user name: ").lower()
+        users_name.append(users_name)
+
+        more = input("Do you want to add another user? (y/n): ")
+        if more.lower() != 'y':
+            break
+    
+    for user in users_name:
+        delete_multiple_users(server_id, user)
 
 
 def delete_ssh_public_key():
@@ -154,7 +304,7 @@ def delete_ssh_public_key():
         print("Error deleting SSH Key: {}".format(e))
 
 
-def update_user():
+""" def update_user():
     server_id = input("Enter Server ID: ").strip()
     user_name = input("Enter User Name: ").strip()
     role_arn = input("Enter Role ARN: ").strip()
@@ -165,17 +315,8 @@ def update_user():
         print("All fields are required.")
         return
     
-    try:
-        transfer_client.update_user(
-            ServerId=server_id,
-            UserName=user_name,
-            RoleArn=role_arn,
-            PolicyArn=policy_arn,
-            HomeDirectory=home_directory
-        )
-        print("User {} updated successfully on Server {}".format(user_name, server_id))
-    except Exception as e:
-        print("Error updating user: {}".format(e))
+    _update_user_logic(server_id, user_name, role_arn, policy_arn, home_directory)
+"""
 
 
 def update_ssh_public_key():
@@ -277,13 +418,13 @@ def handle_transfer_choice(choice):
         case '1':
             create_server()
         case '2':
-            create_user()
+            create_users()
         case '3':
-            delete_user()
+            delete_users()
         case '4':
             delete_ssh_public_key()
         case '5':
-            update_user()
+            update_users()
         case '6':
             update_ssh_public_key()
         case '7':
